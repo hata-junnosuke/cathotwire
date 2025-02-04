7150c5ade54ddb6761736433cd8396e5d6b1bab1
hata-junnosuke <jh19911209@icloud.com>
2025-02-04 13:44:16 +0900

    チュートリアル1完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :owls
+  resources :hedgehogs
+  resources :chicks
+  resources :dogs
+  resources :cats
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,4 +16,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root to: redirect("/cats")
 end
